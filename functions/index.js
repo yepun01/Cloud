1494a6d4338dd861fecf06e422ddaa534d6245d1
jonathanjalmain <jonathan.jalmain@epitech.eu>
2026-04-08 21:40:33 +0200

    feat(backend): wire discordInteraction with secret-bound DISCORD_PUBLIC_KEY (defineSecret + env injection at runtime)

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,21 @@
 const { onRequest } = require("firebase-functions/v2/https");
+const { defineSecret } = require("firebase-functions/params");
 
 const placePixelHandler = require("./src/pixel/placePixel");
 const getCanvasHandler = require("./src/pixel/getCanvas");
+const discordInteractionHandler = require("./src/discord/discordInteraction");
 
 const RUNTIME = { region: "europe-west1", cors: true };
 
+const discordPublicKey = defineSecret("discord-public-key");
+
 exports.placePixel = onRequest(RUNTIME, placePixelHandler);
 exports.getCanvas = onRequest(RUNTIME, getCanvasHandler);
+
+exports.discordInteraction = onRequest(
+  { ...RUNTIME, secrets: [discordPublicKey] },
+  (req, res) => {
+    process.env.DISCORD_PUBLIC_KEY = discordPublicKey.value();
+    return discordInteractionHandler(req, res);
+  }
+);
